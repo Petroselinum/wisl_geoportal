d637ec7a33a4133832a18b6f16075ac2f2e3395e
Petroselinum <petroselinum@proton.me>
2026-09-07 09:44:11 +0200

    reorganizacja_repo

--- WislDbCon.py
+++ WislDbCon.py
@@ -1,23 +1,23 @@
 from sqlmodel import create_engine
 import urllib
+ 
+ 
+def connection(driver='{ODBC Driver 17 for SQL Server}',
+               server='localhost,1433',
+               database='WISL_Baza_zbiorcza',
+               user='sa',
+               password='Fifka125!'):
 
-def connection(driver='{ODBC Driver 17 for SQL Server}', 
-               server='BudniakP', 
-               database='WISL_baza_zbiorcza_IV_cykl_MJ20250226', 
-               trusted_connection='yes'):
-    
-    '''Funkcja zwraca obiekt silnika SQLAlchemy do połączenia z lokalną bazą danych WISL'''
-
-    if any (param == '' for param in [driver, server, database, trusted_connection]):
+ 
+    if any(param in (None, '') for param in [driver, server, database, user, password]):
         raise ValueError("Wszystkie parametry muszą być podane i nie mogą być puste.")
-
+ 
     params = urllib.parse.quote_plus(f'''
-                                     DRIVER={driver};
-                                     SERVER={server};
-                                     DATABASE={database};
-                                     Trusted_Connection={trusted_connection};
-                                     TrustServerCertificate=yes;''')
-    
+                                         DRIVER={driver};
+                                         SERVER={server};
+                                         DATABASE={database};
+                                         UID={user};
+                                         PWD={password};
+                                         TrustServerCertificate=yes;''')
     engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
     return engine
-
